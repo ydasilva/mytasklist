65c1bdd7fca0f71a4978dfaf625594400aa539f4
Youssouf da Silva <youdasilva@Youssoufs-MacBook-Pro.local>
2017-03-07 08:45:41 +0000

    Backend is complete

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,8 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://youssouf:youssouf@ds119810.mlab.com:19810/mytasklist_youssouf',['tasks']);
 
-router.get('/tasks', function(red, res, next){
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
 	//res.send('TASKS API');
 	db.tasks.find(function(err, tasks){
 		if(err){
@@ -12,6 +13,77 @@ router.get('/tasks', function(red, res, next){
 		}
 		res.json(tasks);
 	});
+});
+
+// Get Single Task
+router.get('/tasks/:id', function(req, res, next){
+	//res.send('TASKS API');
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+		if(err){
+			res.send(err);
+		}
+		res.json(task);
+	});
+});
+
+// Save Task
+router.post('/task', function(req, res, next){
+	var task = req.body;
+
+	if(!task.title || (task.isDone + '')){
+		res.status(400);
+		res.json({
+			"error": "Bad Data"
+		});
+	} else {
+		db.tasks.save(task, function(err, task){
+			if(err){
+				res.send(err);
+			}
+			res.json(task);
+		});
+	}
+});
+
+// Delete Task
+router.delete('/tasks/:id', function(req, res, next){
+	//res.send('TASKS API');
+	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+		if(err){
+			res.send(err);
+		}
+		res.json(task);
+	});
+});
+
+// Update Task
+router.put('/tasks/:id', function(req, res, next){
+	var task = req.body;
+	var updTask = {};
+
+	if(task.isDone){
+		updTask.isDone = task.isDone;
+	}
+
+	if(task.title){
+		updTask.title = task.title;
+	}
+
+	if(!updTask){
+		res.status(400);
+		res.json({
+			"error": "Bad Data"
+		});
+	} else {
+		db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+			if(err){
+				res.send(err);
+			}
+			res.json(task);
+		});
+	}
+
+	
 });
 
 module.exports = router;
